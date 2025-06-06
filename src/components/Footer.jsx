cbded75b11e71e460b2a6dbb8be04612d5109603
arielcarlos1 <instalador830@gmail.com>
2025-06-06 10:36:41 -0300

    feat: portfolio responsivo completo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Ariel Carlos. Todos os direitos reservados.</p>
       <div className="social">
-        <a href="https://github.com/Arielcarlos1" target="_blank" rel="noreferrer">GitHub</a>
+        <a href="https://github.com/Arielcarlos1/" target="_blank" rel="noreferrer">GitHub</a>
         <a href="https://www.linkedin.com/in/ariel-carlos-de-souza/" target="_blank" rel="noreferrer">LinkedIn</a>
         {/* Adicione outras redes se quiser */}
       </div>
